462e783d4622d81ad799dc55dae454d57fd07c42
Archilon <Vehemex@gmail.com>
2021-10-06 11:25:53 +0300

    06.10.2021

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// App.tsx
+import React from 'react'
+import MyFirstComponent from './components/myFirstComponent';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <MyFirstComponent />
+        </div>
+    )
+}
+
+export default App
