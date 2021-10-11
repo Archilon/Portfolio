310343a55d552f89c744e7beddb827f8f9c2f973
Jesse Isokääntä <jesseisokaanta@Ohjelmointi-10-MacBook-Pro.local>
2021-10-11 10:47:09 +0300

    latest 11.10.21

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@
 import React from 'react'
 import MyFirstComponent from './components/myFirstComponent';
 import SomeExampleComponent from './components/example/someExampleComponent';
+import ClickCount from './components/ClickCount/clickCount';
 
 const App: React.FC = () => {
     return (
         <div>
             <MyFirstComponent />
+            <ClickCount />
             <SomeExampleComponent />
         </div>
     )
