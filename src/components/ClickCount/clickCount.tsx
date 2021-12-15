735f9344a2c2366c7520617b5ac4c5d06dd03ce2
Archilon <Vehemex@gmail.com>
2021-12-15 15:47:54 +0200

    fixes

--- src/components/ClickCount/clickCount.tsx
+++ src/components/ClickCount/clickCount.tsx
@@ -1,4 +1,4 @@
-import React,  { useState } from 'react'
+import React,{ useState } from 'react'
 import styles from './clickCount.module.scss'
 
 type ClickCountProps = {}
@@ -14,27 +14,40 @@ const percent = (good/(good+neutral+poor)*100||0)
         return (
            
           <div className={styles.container}>
+            <h2>Feedback</h2>
             
             <table>
-              <h2>Feedback</h2>
-              <button onClick={() => setgood(good + 1)}>Good</button>
-              <button onClick={() => setneutral(neutral + 1)}>Neutral</button>
-              <button onClick={() => setpoor(poor + 1)}>Poor</button>
+              <tbody>
               <tr>
+                  <td>
+                    <button onClick={() => setgood(good + 1)}>Good</button>
+                  </td>
+                  <td>
+                    <button onClick={() => setneutral(neutral + 1)}>Neutral</button>
+                  </td>
+                  <td>
+                    <button onClick={() => setpoor(poor + 1)}>Poor</button>
+                  </td>
               </tr>
-                <h2>Statistics</h2>
+              </tbody>
+            </table>
+            <h2>Statistics</h2>
+              <table className={styles.content}>
+                <tbody>
+
                 <tr>
-                <td><p>Good:</p></td>          <td><p>{good} </p></td>
+                <td><p>Good:</p></td><td><p>{good} </p></td>
                 </tr>
                 <tr>
-                <td><p>Neutral:</p></td>          <td><p>{neutral} </p></td>
+                <td><p>Neutral:</p></td><td><p>{neutral} </p></td>
                 </tr>
                 <tr>
-                <td><p>Poor:</p></td>          <td><p>{poor} </p></td>
+                <td><p>Poor:</p></td><td><p>{poor} </p></td>
                 </tr>
                 <tr>
-                <td><p>positive feedback:</p></td>          <td><p>{percent}% </p></td>
+                <td><p>positive feedback:</p></td><td><p>{percent}% </p></td>
                 </tr>
+                </tbody>
             </table>
           </div>
         );
