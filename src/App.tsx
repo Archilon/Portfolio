ff2a96ea7f2c0a0ff6167388aa92b5e935d523f7
Jesse Isokääntä <83401915+Archilon@users.noreply.github.com>
2021-12-15 10:35:13 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,12 @@ import React from 'react'
 import MyFirstComponent from './components/myFirstComponent';
 import SomeExampleComponent from './components/example/someExampleComponent';
 import ClickCount from './components/ClickCount/clickCount';
-
+///<ClickCount />
 const App: React.FC = () => {
     return (
         <div>
             <MyFirstComponent />
-            <ClickCount />
+            
             <SomeExampleComponent />
         </div>
     )
