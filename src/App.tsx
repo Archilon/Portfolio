daedd6d176dc29911f62a9cd171ba483d78bfe74
Archilon <Vehemex@gmail.com>
2022-01-27 13:14:03 +0200

    css and added more info

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 // App.tsx
 import React from 'react'
-import MyFirstComponent from './sites/Front Page/myFirstComponent';
+import Main from './sites/Front Page/Main';
 import Projects from './sites/Projects/Projects';
-import AboutMe from './sites/About me/AboutMe';
-import ClickCount from './components/ClickCount/clickCount';
+import AboutMe from './sites/About_me/AboutMe';
+import Feedback from './sites/Feedback/Feedback';
 import styles from './App.module.scss'
 import {
     HashRouter as Router,
@@ -19,20 +19,18 @@ const App: React.FC = () => {
         <div className={styles.wrapper}>
           <div className={styles.content}>
           <Switch>
-            <Route exact path='/sites/Front Page' component={MyFirstComponent} />
+            <Route exact path='/' component={Main} />
             <Route exact path='/sites/Projects' component={Projects}/>
-            <Route exact path='/sites/About me' component={AboutMe}/>
+            <Route exact path='/sites/About_me' component={AboutMe}/>
           </Switch>
           </div>
         </div>
         <div className={styles.sidebar}>
-          {/* <div className={styles.navbar}> */}
-            <Link to='/sites/Front Page'>Front page</Link>
-            <Link to='/sites/Projects'>Projects</Link>
-            <Link to='/sites/About me'>About me</Link>
-          {/* </div> */}
+            <Link to='/'><button>Front page</button></Link>
+            <Link to='/sites/Projects'><button>Projects</button></Link>
+            <Link to='/sites/About_me'><button>About me</button></Link>
         </div>
-        <div className='Footer'><ClickCount /></div>
+        <div className='Footer'><Feedback /></div>
       </Router>
 
       
